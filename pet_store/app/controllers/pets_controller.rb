e7de9583e57dd2be56d0f68113d48845888186fb
conrad_buys <conradbuys11@gmail.com>
2020-09-15 17:37:27 -0400

    we beasts

--- pet_store/app/controllers/pets_controller.rb
+++ pet_store/app/controllers/pets_controller.rb
@@ -4,8 +4,20 @@ class PetsController < ApplicationController
     end
 
     def index
-        @pets = Pet.all
-        render :index
+        @pets = nil
+        @header_name = params[:species].capitalize
+        temp_species = @header_name[0...-1]
+
+        if temp_species == "Pet"
+            @pets = Pet.all
+        else
+            @pets = Pet.where(species: temp_species)
+            if @pets == [] #if the species was not found in our list of Pets
+                @pets = Pet.all
+                @header_name = "Pets"
+            end
+        end
+        render :index #explicit render. will implicitly render name of method if no explicit render is given
     end
 
     def show
@@ -13,10 +25,25 @@ class PetsController < ApplicationController
         @pet = Pet.find(id)
     end
 
-    def by_species
-        temp_species = params[:species]
-        @all_of_species = Pet.where("species = #{temp_species}")
-        @species_capitalized = temp_species.capitalize + "s"
-    end
+    # def by_species
+    #     temp_species = params[:species]
+    #     @all_of_species = Pet.where("species = ?" params[])
+    #     @species_capitalized = temp_species.capitalize + "s"
+    # end
+
+    # def dogs
+    #     @pets = Pet.where(species: "Dog")
+    #     render :index
+    # end
+
+    # def cats
+    #     @pets = Pet.where(species: "Cat")
+    #     render :index
+    # end
+
+    # def birds
+    #     @pets = Pet.where(species: "Bird")
+    #     render :index
+    # end
 
 end
--- pet_store/config/routes.rb
+++ pet_store/config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #get ____, to: ____
   get '/', to: 'pets#homepage'
-  get '/pets', to: 'pets#index'
+  # get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show', as: 'pet' #pet_path()
-  get '/pets/:species', to: 'pets#by_species', as: 'species' #species_path()
+  #get '/pets/:species', to: 'pets#by_species', as: 'species' #species_path()
+  get "/:species", to: "pets#index"
+  # get '/dogs', to: 'pets#dogs' #rails routes - keep in back of mind
+  # get '/cats', to: 'pets#cats'
+  # get '/birds', to: 'pet#birds'
+  #get ''
 end
